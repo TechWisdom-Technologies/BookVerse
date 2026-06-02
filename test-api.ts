69cc75e8fe8911d0dfed1d83e392768cfd031809
mujahidraj <mujahidraj65@gmail.com>
2026-06-02 20:01:35 +0600

    vercel build error fixed

--- test-api.ts
+++
@@ -1,8 +0,0 @@
-async function main() {
-  const res = await fetch('http://localhost:3000/api/search?q=adven&limit=5');
-  const data = await res.json();
-  data.results.forEach((r: any) => {
-    console.log(`[${r._type}] (Score: ${r.promotionScore || 0}) ${r.title || r.username}`);
-  });
-}
-main();
--- test-avro.ts
+++
@@ -1,6 +0,0 @@
-import { parse } from './src/lib/avro-phonetic';
-
-console.log("adven:", parse("adven"));
-console.log("golpo:", parse("golpo"));
-console.log("amar:", parse("amar"));
-console.log("amr:", parse("amr"));
